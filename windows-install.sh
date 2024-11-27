c36a11dbd5a9db78ad9856e75318d182b38f4c9c
mezbauddin <mezbauddin@gmail.com>
2024-11-27 19:17:01 +0000

    .

--- windows-install.sh
+++ windows-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update -y && apt upgrade -y
+apt update && apt upgrade -y
 apt install grub2 wimtools ntfs-3g -y
 
 # Get the disk size in GB and convert to MB
@@ -10,35 +10,30 @@ disk_size_mb=$((disk_size_gb * 1024))
 # Calculate partition size (25% of total size)
 part_size_mb=$((disk_size_mb / 4))
 
-# Create GPT partition table
+# Create GPT partition table with BIOS Boot Partition
 parted /dev/sda --script -- mklabel gpt
-
-# Create two partitions
-parted /dev/sda --script -- mkpart primary ntfs 1MB ${part_size_mb}MB
-parted /dev/sda --script -- mkpart primary ntfs ${part_size_mb}MB $((2 * part_size_mb))MB
+parted /dev/sda --script -- mkpart primary 1MiB 2MiB
+parted /dev/sda --script -- set 1 bios_grub on
+parted /dev/sda --script -- mkpart primary ntfs 2MiB ${part_size_mb}MiB
+parted /dev/sda --script -- mkpart primary ntfs ${part_size_mb}MiB $((2 * part_size_mb))MiB
 
 # Inform kernel of partition table changes
 partprobe /dev/sda
 sleep 10
 
 # Format the partitions
-mkfs.ntfs -f /dev/sda1
 mkfs.ntfs -f /dev/sda2
+mkfs.ntfs -f /dev/sda3
 
 echo "NTFS partitions created successfully."
 
-# Ensure GPT is properly configured
-echo -e "r\nw\nY\n" | gdisk /dev/sda
-partprobe /dev/sda
-sleep 10
-
 # Mount the first partition
-mount /dev/sda1 /mnt || { echo "Failed to mount /dev/sda1"; exit 1; }
+mount /dev/sda2 /mnt || { echo "Failed to mount /dev/sda2"; exit 1; }
 
 # Prepare directory for Windows disk
 mkdir -p /root/windisk
-umount /dev/sda2 2>/dev/null
-mount /dev/sda2 /root/windisk || { echo "Failed to mount /dev/sda2"; exit 1; }
+umount /dev/sda3 2>/dev/null
+mount /dev/sda3 /root/windisk || { echo "Failed to mount /dev/sda3"; exit 1; }
 
 # Install GRUB
 grub-install --target=i386-pc --boot-directory=/mnt/boot --recheck /dev/sda
@@ -54,8 +49,8 @@ menuentry "windows installer" {
 }
 EOF
 
-# Download Windows Server ISO
-wget -O /root/windisk/winserver.iso "https://software-static.download.prss.microsoft.com/dbazure/888969d5-f34g-4e03-ac9d-1f9786c66749/26100.1742.240906-0331.ge_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"
+# Download Windows Server ISO with resume support
+wget -c -O /root/windisk/winserver.iso "https://software-static.download.prss.microsoft.com/dbazure/888969d5-f34g-4e03-ac9d-1f9786c66749/26100.1742.240906-0331.ge_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"
 if [ ! -s /root/windisk/winserver.iso ]; then
     echo "Failed to download Windows Server ISO. Exiting..."
     exit 1
@@ -93,5 +88,5 @@ touch cmd.txt
 echo 'add virtio /virtio_drivers' > cmd.txt
 wimlib-imagex update boot.wim 2 < cmd.txt
 
-# Reboot
+# Rebooting
 reboot
